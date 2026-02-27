b24c715b8bcf867c9fc34c9cc930f80780849fc5
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2026-02-27 18:14:43 -0500

    Trying to append '>' to options when selected

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 Goals:
 
-- Using malloc and pointers to store data,
+- Using .bin files to store data,
 - Deleting data
 - Some additional stuff...
+
+**DO NOT MODIFY `todo_notes` DIRECTORY**
+
+
+Might make something like this with the **GTK** library.
 
 👍
--- test.c
+++ test.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
-int main(void){
+int main(void) {
+
+  int curr_sel_index = 0, past_sel_index;
+
+  char *options[] = {"TO-DO", "GOALS", "SETTINGS", "EXIT"};
+
+  while (1) {
     
-    char buffer[64] = {0};
-    fgets(buffer, sizeof buffer, stdin);
+    printf("Type an index: ");
+    int input = fgetc(stdin);
+    fgetc(stdin);//Consume "\n"
+    printf("%d\n", input);    
+    if(input>=49 && input <= 52){
+      past_sel_index = curr_sel_index;
+      curr_sel_index = input;
+    }
+    
+    char buffer[36]; 
+    for (int i = 1; i <= 4; i++){
 
-    printf("Your input:\n");
-    fwrite(buffer, sizeof(char), sizeof buffer, stdout);
+      if(curr_sel_index == i){
+        strcpy(buffer,"(>) ");
+        strcat(buffer, options[i]);
+        printf("%s\n", buffer);        
+      } else printf("%s\n", options[i - 1]);
+    } 
+  }
 
-    return 0;
+  return 0;
 }
