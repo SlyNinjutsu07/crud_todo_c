3b186be14f0d1b9513f647629adb2da93de3d798
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2026-02-27 20:17:48 -0500

    I suck at debugging; Fixed and refactored input

--- test.c
+++ test.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <termios.h>
-#include <unistd.h>
 
 int main(void) {
 
-  int curr_sel_index = 0, past_sel_index;
+  int curr_sel_index = 1, past_sel_index = 0;
 
   char *options[] = {"TO-DO", "GOALS", "SETTINGS", "EXIT"};
 
   while (1) {
-    
+
     printf("Type an index: ");
-    int input = fgetc(stdin);
-    fgetc(stdin);//Consume "\n"
-    printf("%d\n", input);    
-    if(input>=49 && input <= 52){
+    int input = fgetc(stdin) - '0'; // To match inputs rather than ascii code
+    fgetc(stdin); // Consume "\n"
+
+    if (1 <= input && input <= 4) {
       past_sel_index = curr_sel_index;
       curr_sel_index = input;
-    }
-    
-    char buffer[36]; 
-    for (int i = 1; i <= 4; i++){
+    } else if(input == 0) exit(0);
 
-      if(curr_sel_index == i){
-        strcpy(buffer,"(>) ");
-        strcat(buffer, options[i]);
-        printf("%s\n", buffer);        
-      } else printf("%s\n", options[i - 1]);
-    } 
+    char buffer[36];
+    for (int i = 1; i <= 4; i++) {
+      if (curr_sel_index == i) {
+        strcpy(buffer, "(>) ");
+        strcat(buffer, options[i - 1]);
+        printf("%s\n", buffer);
+      } else
+        printf("%s\n", options[i - 1]);
+    }
+    memset(buffer, 0, sizeof(buffer));
   }
 
   return 0;
